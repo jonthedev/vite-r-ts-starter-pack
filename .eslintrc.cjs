ac71d05a368bfd97f8f0ab06d3cb59cd8e735b7d
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-01-07 22:06:28 +0100

    fix: lint-staged prettier now lints test scripts refactor: move prettier settings into eslintrc.cjs chore: remove redundant .prettierrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,25 +2,41 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react-hooks/recommended',
+    'prettier'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "*.config.ts", "*.config.js", "commitlint.config.cjs"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    '*.config.ts',
+    '*.config.js',
+    'commitlint.config.cjs'
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
   },
-  plugins: ["react-refresh", "@typescript-eslint", "prettier"],
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
     ],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "prettier/prettier": "error",
-  },
-};
+    'no-unused-vars': 'off',
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all'
+      }
+    ]
+  }
+}
