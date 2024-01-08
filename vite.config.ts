7722844b5785cc474aeca11a915cd6695180d236
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-01-08 20:57:54 +0100

    feat: add and configure vite-plugin-checker for dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,37 @@ import path from "node:path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
+import checker from "vite-plugin-checker"
+
+//TODO: CONSIDER ENV VARIABLES FOR LINT-STAGE/FORMAT SCRIPTS
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    !process.env.VITEST
+      ? checker({
+          typescript: true,
+          eslint: {
+            lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+          },
+        })
+      : undefined,
+  ],
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
       "@db": path.resolve(__dirname, "./src/db"),
-      "@theme": path.resolve(__dirname, "./src/theme")
-    }
+      "@theme": path.resolve(__dirname, "./src/theme"),
+    },
   },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setup.ts",
-    css: true
-  }
+    css: true,
+  },
 })
