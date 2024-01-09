b6166ec34bb7c069b3b7504ef9fb14d32a00b0f9
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-01-09 11:56:32 +0100

    perf: lockdown node version for project

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,6 @@ import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 import checker from "vite-plugin-checker"
 
-//TODO: CONSIDER ENV VARIABLES FOR LINT-STAGE/FORMAT SCRIPTS
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
