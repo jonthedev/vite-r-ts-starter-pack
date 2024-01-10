78144dd616c963657897df79cf217730bd4bc05d
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-01-10 21:05:01 +0100

    chore: clean out assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import SectionTitle from "@src/components/Typography/Title/SectionTitle"
-import Hacker from "@src/components/SVGR/Hacker"
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <SectionTitle title="This is a title"></SectionTitle>
-      <Hacker />
     </div>
   )
 }
